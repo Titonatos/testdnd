ddfa9574a289a3c6cc675f37ce3d33aa9a651dd2
Titonatos <titanium4ik777@gmail.com>
2025-05-14 18:59:18 +0300

    minor visual updates

--- entities/field/ui.js
+++ entities/field/ui.js
@@ -2,9 +2,14 @@ export function renderField(field) {
     const fieldElement = document.createElement('div');
     fieldElement.className = 'grid-item';
     fieldElement.setAttribute('data-id', field.id);
-    const burgerIcon = document.createElement('span');
-    burgerIcon.textContent = '≡';
+    
+    const burgerIcon = document.createElement('img');
+    burgerIcon.src = '/shared/icons/burgerMenu.svg';
+    burgerIcon.alt = 'burgerIcon';
     burgerIcon.style.marginRight = '8px';
+    burgerIcon.style.width = '20px';
+    burgerIcon.style.height = '20px';
+
     fieldElement.appendChild(burgerIcon);
     fieldElement.appendChild(document.createTextNode(field.name + ' - ' + field.text));
     fieldElement.draggable = true;
